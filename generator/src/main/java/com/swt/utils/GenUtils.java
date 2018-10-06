06f5a326b4421fab1c3a4ecd9fd739698a0729e1
rongxuanlong <786086808@qq.com>
2018-10-06 14:54:28 +0800

    - 删除无用jar包 * 修复无所有字段出现未转义的vm文件

--- generator/src/main/java/com/swt/utils/GenUtils.java
+++ generator/src/main/java/com/swt/utils/GenUtils.java
@@ -69,6 +69,7 @@ public class GenUtils {
         String sql = null;
         String mainDirectory = config.getString("mainDirectory") + "//";
         boolean hasBigDecimal = false;
+        boolean search = false;
         //表信息
         TableEntity tableEntity = new TableEntity();
         tableEntity.setTableName(table.get("tableName"));
@@ -129,6 +130,7 @@ public class GenUtils {
                 for (String s : searchKey) {
                     if(columnName.indexOf(s) > -1 ){
                         tableEntity.setSearchColumn(columnEntity);
+                        search = true;
                         break;
                     }
                 }
@@ -183,7 +185,7 @@ public class GenUtils {
         map.put("swagger", config.getBoolean("swagger",false));
 
         //是否添加查询字段
-        map.put("search", config.getBoolean("search",false));
+        map.put("search", search&&config.getBoolean("search",false));
 
         //作者信息配置
         map.put("author", config.getString("author"));
