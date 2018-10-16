d6298da6bbc0509557df407bc6eb3b0ba7adbfbe
rongxuanlong <786086808@qq.com>
2018-10-16 17:58:58 +0800

    + 多数据源新增绑定路径

---
+++ admin/src/main/java/com/swt/datasources/aspect/DataSourceAop.java
@@ -0,0 +1,41 @@
+package com.swt.datasources.aspect;
+
+import com.swt.datasources.DataSourceNames;
+import com.swt.datasources.DynamicDataSource;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * desc: 数据源切面配置 指定文件
+ * Created by jack-cooper on 2017/1/18.
+ */
+//@Aspect
+//@Order(-1)
+//@Component
+public class DataSourceAop {
+
+    Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Before("execution(* com.swt.modules.*.service..*.*(..))")
+    public void setCenterUserDataSourceType() {
+        DynamicDataSource.setDataSource(DataSourceNames.FIRST);
+        log.info("dataSource == >:" + DataSourceNames.FIRST);
+    }
+
+//    @Before("execution(* com.swt.modules.*.service..*.*(..))")
+//    public void setUserCenterDataSourceType(){
+//        DynamicDataSource.setDataSource(DataSourceNames.SECOND);
+//        log.info("dataSource == >：userCenter");
+//    }
+
+    @After("execution(* com.swt.modules.*.service.*..*.*(..)) ")
+    public void afterReturning() throws Throwable {
+        DynamicDataSource.clearDataSource();
+        log.info("=====> clear dataSource aop ");
+    }
+}
