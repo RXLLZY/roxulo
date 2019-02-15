ba74bee14ec7e48f2468375ba8d3a6cbcaedd3da
rongxuanlong <786086808@qq.com>
2019-02-15 10:58:46 +0800

    删除无用的东西

--- generator/src/main/java/com/swt/utils/GenUtils.java
+++ generator/src/main/java/com/swt/utils/GenUtils.java
@@ -69,7 +69,6 @@ public class GenUtils {
         String sql = null;
         String mainDirectory = config.getString("mainDirectory") + "//";
         boolean hasBigDecimal = false;
-        boolean search = false;
         //表信息
         TableEntity tableEntity = new TableEntity();
         tableEntity.setTableName(table.get("tableName"));
@@ -192,7 +191,7 @@ public class GenUtils {
         map.put("swagger", config.getBoolean("swagger",false));
 
         //是否添加查询字段
-        map.put("search", search&&config.getBoolean("search",false));
+        map.put("search", config.getBoolean("search",false));
 
         //作者信息配置
         map.put("author", config.getString("author"));
