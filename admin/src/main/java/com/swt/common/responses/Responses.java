a497dc4fa069034340f3699db74a2c33f1c0ea56
rongxuanlong <786086808@qq.com>
2019-02-18 16:54:09 +0800

    去除lombok相关代码

--- admin/src/main/java/com/swt/common/responses/Responses.java
+++ admin/src/main/java/com/swt/common/responses/Responses.java
@@ -38,7 +38,7 @@ public class Responses<T> {
     /**
      * 不需要返回结果
      *
-     * @param status
+     * @param status http状态码
      */
     public static Responses<Void> success(HttpServletResponse response, HttpStatus status) {
         response.setStatus(status.value());
@@ -49,7 +49,7 @@ public class Responses<T> {
     /**
      * 成功返回
      *
-     * @param object
+     * @param object 返回数据
      */
     public static <T> Responses<T> success(HttpServletResponse response, T object) {
         return success(response, HttpStatus.OK, object);
@@ -59,8 +59,8 @@ public class Responses<T> {
     /**
      * 成功返回
      *
-     * @param status
-     * @param object
+     * @param status http状态码
+     * @param object 返回数据
      */
     public static <T> Responses<T> success(HttpServletResponse response, HttpStatus status, T object) {
         response.setStatus(status.value());
@@ -88,31 +88,34 @@ public class Responses<T> {
         return "Responses(status=" + this.getStatus() + ", result=" + this.getResult() + ")";
     }
 
-    public static class ResponsesBuilder<T>{
-
+    public static class ResponsesBuilder<T> {
         private Integer status;
         private T result;
 
         ResponsesBuilder() {
         }
 
-        public Responses.ResponsesBuilder status(Integer status) {
+        public Responses.ResponsesBuilder<T> status(Integer status) {
             this.status = status;
             return this;
         }
 
-        public Responses.ResponsesBuilder result(T object) {
+        public Responses.ResponsesBuilder<T> result(T result) {
             this.result = result;
             return this;
         }
 
-        public Responses <T>build() {
+        public Responses<T> build() {
             return new Responses(this.status, this.result);
         }
 
+        @Override
+        public String toString() {
+            return "Responses.ResponsesBuilder(status=" + this.status + ", result=" + this.result + ")";
+        }
     }
 
-    public static ResponsesBuilder builder() {
+    public static <T> ResponsesBuilder<T> builder() {
         return new ResponsesBuilder();
     }
 
